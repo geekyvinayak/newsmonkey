42dfa2e0be092aecd6b31782a5085d1ce68d1cd1
Vinayak Pandey <geekyvinayak@gmail.com>
2023-07-04 22:25:05 +0530

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
+import News from './Components/News';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <>
+    <Router>
+     <Navbar></Navbar>
+     <Routes>
+          <Route exact path="/" element={<News  key="general" country="in" category="general"/>}/> 
+          <Route exact path="/business" element={<News  key="business" country="in" category="business"/>}/>
+          <Route exact path="/entertainment" element={<News key="entertainment" country='in' category="entertainment"></News>}/>
+          <Route exact path="/health" element={<News key="health" country='in' category="health"></News>}/>
+          <Route exact path="/science" element={<News key="science" country='in' category="science"></News>}/>
+          <Route exact path="/sports" element={<News key="sports" country='in' category="sports"></News>}/>
+          <Route exact path="/technology"element={<News key="technology" country='in' category="technology"></News>}/>
+      </Routes>
+    </Router>  
+    </>
+    )
+  }
 }
 
-export default App;
+export default App
